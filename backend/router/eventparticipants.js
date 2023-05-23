85c28291c0837a5ee674e21f1ee13e1b9b55bb71
vaidabernotiene <vaidabernotiene@yahoo.com>
2023-05-23 18:35:48 +0300

    on all router verifyToken

--- backend/router/eventparticipants.js
+++ backend/router/eventparticipants.js
@@ -3,16 +3,17 @@ const router = express.Router();
 
 const { dbConnection } = require("../db");
 const { defaultCallback } = require("../utills/dbUtills");
+const { verifyToken } = require("../utills/authenticationUtills");
 
 // GET ------------------------------------------------------------
-router.get("/participants_list", (req, res) => {
+router.get("/participants_list", verifyToken, (req, res) => {
   dbConnection.execute(`SELECT * FROM eventusers`, (err, result) =>
     defaultCallback(err, result, res)
   );
 });
 
 // POST ------------------------------------------------------------
-router.post("/new_participant", (req, res) => {
+router.post("/new_participant", verifyToken, (req, res) => {
   const {
     body: { name, surname, email, phone },
   } = req;
@@ -25,29 +26,29 @@ router.post("/new_participant", (req, res) => {
 });
 
 // PUT -------------------------------------------------------------
-router.put("/new_participant/:id", (req, res) => {
-    const { id } = req.params;
-    const {
-      body: { name, surname, email, phone },
-    } = req;
-  
-    dbConnection.execute(
-      "UPDATE eventusers SET name=?, surname=?, email=?, phone=? WHERE id=?",
-      [name, surname, email, phone, id],
-      (err, result) => defaultCallback(err, result, res)
-    );
-  });
+router.put("/new_participant/:id", verifyToken, (req, res) => {
+  const { id } = req.params;
+  const {
+    body: { name, surname, email, phone },
+  } = req;
+
+  dbConnection.execute(
+    "UPDATE eventusers SET name=?, surname=?, email=?, phone=? WHERE id=?",
+    [name, surname, email, phone, id],
+    (err, result) => defaultCallback(err, result, res)
+  );
+});
 
 // DELETE -----------------------------------------------------------
-router.delete("/participants_list/:id", (req, res) => {
-    const { id } = req.params;
-  
-    dbConnection.execute(
-      "DELETE FROM eventusers WHERE id=?",
-      [id],
-      (err, result) => defaultCallback(err, result, res)
-    );
-  });
+router.delete("/participants_list/:id", verifyToken, (req, res) => {
+  const { id } = req.params;
+
+  dbConnection.execute(
+    "DELETE FROM eventusers WHERE id=?",
+    [id],
+    (err, result) => defaultCallback(err, result, res)
+  );
+});
 
 // EXPORTS --------------------------------------------------
 module.exports = router;
