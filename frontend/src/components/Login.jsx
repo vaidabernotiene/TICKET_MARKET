82d4f385ca197ff51308b59f3434b92a2bdd0d5f
vaidabernotiene <vaidabernotiene@yahoo.com>
2023-05-23 19:53:29 +0300

    correct Navbar loging is protected or not

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -1,9 +1,45 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useState, useContext } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import axios from "axios";
+
+import { AuthenticationContext } from "./AuthenticationContext";
 import Button from "./Button";
 import "../App.css";
 
 export const Login = () => {
+  const { setIsSignedIn } = useContext(AuthenticationContext);
+  const [formData, setFormData] = useState({
+    email: "",
+    password: "",
+  });
+  const [error, setError] = useState(null);
+  const navigate = useNavigate();
+
+  const handleOnSubmit = (e) => {
+    e.preventDefault();
+
+    axios
+      .post("http://localhost:5000/login", formData)
+      .then((response) => {
+        if (response.data.token) {
+          localStorage.setItem("token", response.data.token);
+          setIsSignedIn(true);
+          navigate("/home");
+          console.log(response.data);
+        } else {
+          setError(response.data.message);
+        }
+      })
+      .catch((err) => console.log(err));
+  };
+
+  const handleOnChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value,
+    });
+  };
+
   return (
     <div className="bg-blue-gradient flex justify-center items-center h-screen container">
       <div className="w-1/2 p-6 border rounded shadow-lg">
@@ -15,6 +51,7 @@ export const Login = () => {
               id="email"
               name="email"
               placeholder="Email"
+              onChange={handleOnChange}
               className="w-full border px-4 py-2 rounded"
             />
           </div>
@@ -24,12 +61,16 @@ export const Login = () => {
               id="password"
               name="password"
               placeholder="Password"
+              onChange={handleOnChange}
               className="w-full border px-4 py-2 rounded"
             />
           </div>
-          <Button text='Login'/>
+          <Button text="Login" onClick={handleOnSubmit} />
         </form>
-        <p className="pt-5">Need an Account? Click <Link to="/register">HERE</Link></p>
+        {error && <div>{error}</div>}
+        <p className="pt-5">
+          Need an Account? Click <Link to="/register">HERE</Link>
+        </p>
       </div>
     </div>
   );
---
+++ frontend/src/components/Protected.jsx
@@ -0,0 +1,41 @@
+import { useContext, useEffect } from "react";
+import { Outlet, Navigate, useNavigate, useLocation } from "react-router-dom";
+import axios from "axios";
+import { AuthenticationContext } from "./AuthenticationContext";
+
+const Protected = ({ isLoading, setIsLoading }) => {
+  const { isSignedIn, setIsSignedIn } = useContext(AuthenticationContext);
+  const { setUsername } = useContext(AuthenticationContext);
+  const navigate = useNavigate();
+  const location = useLocation();
+
+  useEffect(() => {
+    const token = localStorage.getItem("token");
+    axios
+      .get("http://localhost:5000/token/verify", {
+        headers: {
+          authorization: "Bearer " + token,
+        },
+      })
+      .then((response) => {
+        if (response.data.id) {
+          setIsSignedIn(true);
+          navigate(location.pathname);
+          setIsLoading(false);
+          setUsername(response.data.name);
+        }
+      });
+  }, [navigate, setIsSignedIn, setIsLoading, location.pathname, setUsername]);
+
+  if (isLoading) {
+    return <div>Page is loading...</div>;
+  }
+
+  if (!isSignedIn) {
+    return <Navigate to="/login" />;
+  }
+
+  return <Outlet />;
+};
+
+export default Protected;
